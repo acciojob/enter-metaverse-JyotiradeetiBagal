5f373f71544522d0c8aa687794429da5360fce3f
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-06 16:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let btn=document.getElementById("enterBtn");
+let txt=document.getElementById("status");
+
+btn.addEventListener("click",()=>{
+	txt.innerHTML="<h1>Entered Metaverse</h1>";
+});
